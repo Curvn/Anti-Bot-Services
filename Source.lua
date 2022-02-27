5e4b2b1ccc7e0a9c0a1f8be754a5f420477d80b1
Curvn <50517248+Curvn@users.noreply.github.com>
2022-02-27 12:33:32 -0500

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,30 @@
+local SR = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
 
+game.Players.PlayerAdded:Connect(function(Player)
+    local Data = syn.request({
+        Url = "https://raw.githubusercontent.com/Curvn/Anti-Bot-Services/main/BL",
+        Method = "GET"
+    })
+	local LC = string.split(Data.Body, ";")
+	print(LC)
+	if table.find(LC, tostring(Player.UserId)) then
+	    print("BOT HAS JOINED: " .. Player.Name .. " | " .. Player.UserId)
+	    SR:FireServer("/mute "..Player.Name, "All")
+		wait(2)
+		if Player.Character:FindFirstChild("ForceField") then Player.Character.ForceField:Destroy() end
+		for i,v in pairs(Player.Character:GetDescendants()) do
+            if v:IsA("BasePart") then
+                v:Destroy()
+            end
+        end
+        Player.CharacterAdded:Connect(function(Char)
+            wait()
+            if Char:FindFirstChild("ForceField") then Char.ForceField:Destroy() end
+            for i,v in pairs(Char:GetDescendants()) do
+                if v:IsA("BasePart") then
+                    v:Destroy()
+                end
+            end
+        end)
+	  end
+end)
